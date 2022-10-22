53afdf96e55201af8831adef8688f1fe19e8aee5
tanvikinjale <you@example.com>
2022-10-22 16:16:14 +0530

    added idea components

---
+++ src/components/IdeaList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import IdeaPost from './IdeaPost';
+
+function IdeaList() {
+  return (
+    <div>
+        <IdeaPost
+          name="Tanvi Kinjale"
+          gmail="tanvikinjale@gmail.com"
+          title="Idea title"
+          description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem ex, officia temporibus animi similique ullam, eius provident, nam repellat ad quia? Aliquam, rem aperiam alias tempore voluptatibus ad consectetur rerum?
+
+           "
+          imgUrl="https://pbs.twimg.com/profile_images/1121328878142853120/e-rpjoJi_bigger.png"
+        />
+    </div>
+  )
+}
+
+export default IdeaList
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'orange':'#EFA451',
+        'blue':'#013BBA',
+        'lb':'#2a5fd4',
+        'grey':'#7587A4',
+        'offwhite':'#FAF9F6',
+        'redish':'#EF4A37'
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        cambria:["Cambria", "serif"]
+      },
+    },
+  },
+  plugins: [],
+}
